a60cb5a20fba1b7d5127057cf2ec6beef08b4fdb
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-18 17:56:25 +0200

    destini: do step 28

--- destini/lib/src/story_page.dart
+++ destini/lib/src/story_page.dart
@@ -70,8 +70,8 @@ class _StoryPageState extends State<StoryPage> {
             ),
             Expanded(
               flex: 2,
-              // TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
               child: Visibility(
+                visible: widget.storyBrain.buttonShouldBeVisible(),
                 child: TextButton(
                   onPressed: () {
                     // Choice 2 made by user.
