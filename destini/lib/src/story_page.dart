80560cdf8d062838cbfdf39c73bdc29c7a73221b
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-18 17:50:11 +0200

    destini: do step 24

--- destini/lib/src/story_page.dart
+++ destini/lib/src/story_page.dart
@@ -47,7 +47,9 @@ class _StoryPageState extends State<StoryPage> {
               child: TextButton(
                 onPressed: () {
                   // Choice 1 made by user.
-                  widget.storyBrain.nextStory(1);
+                  setState(() {
+                    widget.storyBrain.nextStory(1);
+                  });
                 },
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all<Color>(
@@ -73,7 +75,9 @@ class _StoryPageState extends State<StoryPage> {
               child: TextButton(
                 onPressed: () {
                   // Choice 2 made by user.
-                  widget.storyBrain.nextStory(2);
+                  setState(() {
+                    widget.storyBrain.nextStory(2);
+                  });
                 },
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all<Color>(
@@ -94,7 +98,5 @@ class _StoryPageState extends State<StoryPage> {
       ),
     );
   }
-// TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
 // TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
 }
