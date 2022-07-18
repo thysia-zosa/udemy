4761f6dfc979f9abb10930949a8187c8d7a640ec
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-18 16:59:33 +0200

    destini: do step 3

--- destini/lib/src/story.dart
+++ destini/lib/src/story.dart
@@ -1,5 +1,12 @@
 class Story {
-// TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+  /// stores the story text.
+  final String storyTitle;
+
+  /// stores the text for choice 1.
+  final String choice1;
+
+  /// stores the text for choice 2.
+  final String choice2;
 
 // TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
 }
