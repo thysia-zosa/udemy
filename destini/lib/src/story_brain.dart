d849bfee380773d82fedb40bbf60c47b675d594e
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-18 17:10:22 +0200

    destini: do step 12

--- destini/lib/src/story_brain.dart
+++ destini/lib/src/story_brain.dart
@@ -41,7 +41,8 @@ class StoryBrain {
   /// returns the text for the first choice1 from _storyData.
   String getChoice1() => _storyData.first.choice1;
 
-// TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
+  /// returns the text for the first choice2 from _storyData.
+  String getChoice2() => _storyData.first.choice2;
 
 // TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
