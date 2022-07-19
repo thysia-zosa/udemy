bf1dbce36aaf408f70b06d2607179d6bb3dcdd13
Thysia.Zosa <thysia.zosa@gmail.com>
2022-07-19 17:08:44 +0200

    add bmi person stuff and calculations

---
+++ bmi_calculator/lib/src/models/bmi_person.dart
@@ -0,0 +1,53 @@
+import 'bmi_result.dart';
+import 'gender.dart';
+
+List<BmiResult> _results = [
+  BmiResult(
+    18.0,
+    19.0,
+    'Underweight'.toUpperCase(),
+    'You have a higher than normal body weight. Try to exercise more.'
+        .toUpperCase(),
+  ),
+  BmiResult(
+    25.0,
+    26.0,
+    'Normal weight'.toUpperCase(),
+    'You have a normal body weight. Good job!'.toUpperCase(),
+  ),
+  BmiResult(
+    double.infinity,
+    double.infinity,
+    'Overweight'.toUpperCase(),
+    'You have a lower than normal body weight. You can eat a bit more.'
+        .toUpperCase(),
+  ),
+];
+
+class BmiPerson {
+  double height;
+  int weight;
+  int age;
+  Gender? gender;
+
+  BmiPerson({
+    this.height = 180,
+    this.weight = 70,
+    this.age = 25,
+  });
+
+  double _calculateBmi() {
+    return 10000 * weight / (height * height);
+  }
+
+  BmiResult getResult() {
+    if (gender == null) throw Exception('Gender is null');
+    double bmi = _calculateBmi();
+    return _results.firstWhere((element) =>
+        (gender == Gender.male
+            ? element.upperMaleConstraint
+            : element.upperFemaleConstraint) >
+        bmi)
+      ..bmi = bmi.toStringAsFixed(1);
+  }
+}
---
+++ bmi_calculator/lib/src/models/bmi_result.dart
@@ -0,0 +1,14 @@
+class BmiResult {
+  double upperMaleConstraint;
+  double upperFemaleConstraint;
+  String title;
+  String explanation;
+  String bmi = '';
+
+  BmiResult(
+    this.upperMaleConstraint,
+    this.upperFemaleConstraint,
+    this.title,
+    this.explanation,
+  );
+}
