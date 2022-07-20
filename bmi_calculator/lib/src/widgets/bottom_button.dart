2ef0c1d6c3127912a8f64cbe17725f6e23be11dc
Thysia.Zosa <thysia.zosa@gmail.com>
2022-07-20 08:00:22 +0200

    implement bottom button

---
+++ bmi_calculator/lib/src/widgets/bottom_button.dart
@@ -0,0 +1,39 @@
+import 'package:bmi_calculator/src/consts.dart';
+import 'package:flutter/material.dart';
+
+const _height = 80.0;
+const _marginTop = 10.0;
+const _paddingBottom = 20.0;
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.only(
+          top: _marginTop,
+        ),
+        width: double.infinity,
+        color: Cols.red,
+        height: _height,
+        padding: EdgeInsets.only(
+          bottom: _paddingBottom,
+        ),
+        child: Text(
+          text,
+          style: Texts.button,
+        ),
+      ),
+      onTap: onPressed,
+    );
+  }
+}
