81129b370ff6cc337fb69201c08199e00cecaa96
Thysia.Zosa <thysia.zosa@gmail.com>
2022-07-20 08:35:36 +0200

    implement icon card

---
+++ bmi_calculator/lib/src/widgets/icon_card.dart
@@ -0,0 +1,35 @@
+import 'package:bmi_calculator/src/consts.dart';
+import 'package:flutter/material.dart';
+
+const double _iconSize = 80.0;
+const double _distance = 15.0;
+
+class IconCard extends StatelessWidget {
+  const IconCard({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          size: _iconSize,
+        ),
+        SizedBox(
+          height: _distance,
+        ),
+        Text(
+          text,
+          style: Texts.label,
+        ),
+      ],
+    );
+  }
+}
