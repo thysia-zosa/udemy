02cd6ec91beffb75c56e8442054898d4e11fe880
Thysia.Zosa <thysia.zosa@gmail.com>
2022-07-24 21:28:06 +0200

    update constants

--- clima/lib/src/utilities/consts.dart
+++ clima/lib/src/utilities/consts.dart
@@ -4,6 +4,10 @@ abstract class ClimaLits {
   static const String _spartanFont = 'Spartan';
   static const String locationBG = 'assets/images/location_background.jpg';
   static const String cityBG = 'assets/images/city_background.jpg';
+  static const String url = 'api.openweathermap.org';
+  static const String path = '/data/2.5/weather';
+  static const String units = 'metric';
+  static const String apiKey = '41424e6874a9bdc5039cc4c3cc6f0beb';
 }
 
 abstract class ClimaTStyle {
