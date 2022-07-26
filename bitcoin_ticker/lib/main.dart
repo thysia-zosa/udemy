28564a2235455ded25e0f3d8ae4698c751c709b5
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-26 19:06:11 +0200

    start bitcoin ticker project

---
+++ bitcoin_ticker/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'src/bitcoin_ticker.dart';
+
+void main() => runApp(const BitcoinTicker());
---
+++ bitcoin_ticker/lib/src/bitcoin_ticker.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/price_screen.dart';
+
+class BitcoinTicker extends StatelessWidget {
+  const BitcoinTicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: PriceScreen(),
+    );
+  }
+}
