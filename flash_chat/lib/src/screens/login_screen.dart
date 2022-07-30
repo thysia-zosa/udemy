5ca93d3fed9a675004995518095aa184acbb1480
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-30 14:30:20 +0200

    renamed user entry scaffold file

--- flash_chat/lib/src/screens/login_screen.dart
+++ flash_chat/lib/src/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../backend/backend.dart';
-import '../widgets/user_entry.dart';
+import '../widgets/user_entry_scaffold.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
