0bf34fe9312fa9b240eab24f7f8bb1b674e38dc5
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-30 13:43:29 +0200

    add backend functionality

--- flash_chat/lib/src/backend/backend.dart
+++ flash_chat/lib/src/backend/backend.dart
@@ -1,7 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Backend {
   static Backend? _instance;
+  final FirebaseAuth _auth;
 
-  Backend._();
+  Backend._() : _auth = FirebaseAuth.instance;
 
   factory Backend() => _instance ??= Backend._();
+
+  Future<String?> register({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final UserCredential credential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future<String?> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
 }
