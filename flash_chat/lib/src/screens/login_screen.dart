ef3468415f3679083e2d37c2af28405e21626354
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-07-30 14:28:16 +0200

    refactor login screen

--- flash_chat/lib/src/screens/login_screen.dart
+++ flash_chat/lib/src/screens/login_screen.dart
@@ -1,72 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/chat_button.dart';
-import '../widgets/chat_text_field.dart';
-import '../widgets/hero_icon.dart';
+import '../backend/backend.dart';
+import '../widgets/user_entry.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   static String route = 'loginScreen';
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  String _email = '';
-  String _password = '';
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24.0,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const HeroIcon(
-              height: 200.0,
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            ChatTextField(
-              hintText: 'Enter your email',
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                _email = value;
-              },
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            ChatTextField(
-              hintText: 'Enter your password',
-              obscureText: true,
-              onChanged: (value) {
-                _password = value;
-              },
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            ChatButton(
-              color: Colors.lightBlueAccent,
-              onPressed: _login,
-              text: 'Log In',
-            ),
-          ],
-        ),
-      ),
+    return UserEntryScaffold(
+      text: 'Log In',
+      color: Colors.lightBlueAccent,
+      callBack: Backend().login,
     );
-  }
-
-  Future<void> _login() async {
-    // TODO: implement login functionality.
   }
 }
