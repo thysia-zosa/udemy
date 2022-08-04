63aaf7d5456063d9f49d98032be84cc8e9bf88d1
Jonas Tochtermann <thysia.zosa@gmail.com>
2022-08-04 10:21:23 +0200

    restyle bottom widget

--- todoey/lib/src/widgets/add_task_bottom_sheet.dart
+++ todoey/lib/src/widgets/add_task_bottom_sheet.dart
@@ -10,25 +10,59 @@ class AddTaskBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String taskTitle = '';
+
     return Container(
-      padding: EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          Text('Add Task: Title'),
-          TextField(
-            autofocus: true,
-            onChanged: (value) => taskTitle = value,
+      color: const Color(0xff757575),
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
           ),
-          TextButton(
-            onPressed: () {
-              Provider.of<TaskData>(context).addTask(
-                Task(title: taskTitle),
-              );
-              Navigator.pop(context);
-            },
-            child: Text('Add Task'),
-          ),
-        ],
+        ),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add a title to the new task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              autofocus: true,
+              onChanged: (value) => taskTitle = value,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            TextButton(
+              onPressed: () {
+                Provider.of<TaskData>(context, listen: false).addTask(
+                  Task(title: taskTitle),
+                );
+                Navigator.pop(context);
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Colors.lightBlueAccent,
+                ),
+              ),
+              child: const Text(
+                'Add Task',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
